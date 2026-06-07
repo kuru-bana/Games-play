ec64f81300426c6f92e06c92fd030d74c5078e6e
kuru-bana <kuru-api@hotmail.com>
2026-06-07 11:06:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'public', 'Brainrot')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', 'Brainrot', 'index.html'));
+  res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Brainrot Games Player running on port ${PORT}`);
+  console.log(`Game Portal running on port ${PORT}`);
 });
